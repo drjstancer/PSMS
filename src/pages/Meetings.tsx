d09a7c040059e5727242f06f92cf7e22abeed348
drjstancer <drj.stancer@gmail.com>
2026-06-16 19:04:37 -0500

    Add meetings page foundation

---
+++ src/pages/Meetings.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Meetings() {
+  return (
+    <div>
+      <h1>Meetings</h1>
+      <p>Create and manage advising and mentor meeting records.</p>
+    </div>
+  );
+}

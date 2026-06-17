fe526e86218897b0d285a4a324ebe47ce1c345a5
drjstancer <drj.stancer@gmail.com>
2026-06-17 15:06:14 -0500

    Add participant search foundation

---
+++ src/components/ParticipantSearch.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ParticipantSearch() {
+  return (
+    <div>
+      <input placeholder='Search by Name, MU ID, PawPrint, Program, or Cohort' />
+    </div>
+  );
+}

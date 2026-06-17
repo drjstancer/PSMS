b52fe559d118130ff3207b92962718bd933b5bfa
drjstancer <drj.stancer@gmail.com>
2026-06-16 19:03:51 -0500

    Add participants page foundation

---
+++ src/pages/Participants.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Participants() {
+  return (
+    <div>
+      <h1>Participants</h1>
+      <p>Search participants by Name, MU ID, PawPrint, Program, or Cohort.</p>
+    </div>
+  );
+}

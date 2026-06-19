70e4ec7acbf91ae18266e41de1530f5802f2918b
drjstancer <drj.stancer@gmail.com>
2026-06-19 13:30:30 -0500

    Add progress card component

---
+++ src/components/ProgressCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ProgressCard() {
+  return (
+    <div>
+      <h3>Progress</h3>
+      <p>Overall GPA, Science GPA, Math/Science GPA, and requirements.</p>
+    </div>
+  );
+}

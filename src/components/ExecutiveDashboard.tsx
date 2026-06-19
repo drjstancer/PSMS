b542fdaa2841d8dd9fd2340122d1ed5ebe6de198
drjstancer <drj.stancer@gmail.com>
2026-06-19 13:26:17 -0500

    Add executive dashboard component

---
+++ src/components/ExecutiveDashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ExecutiveDashboard() {
+  return (
+    <div>
+      <h2>Executive Dashboard</h2>
+      <p>Total Participants • Program Trends • Attendance Trends • Meeting Trends • Interventions</p>
+    </div>
+  );
+}

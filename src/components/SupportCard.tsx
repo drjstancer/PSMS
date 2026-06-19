bf13711af0f78c9afa99fd73e52906e7ba776552
drjstancer <drj.stancer@gmail.com>
2026-06-19 13:29:59 -0500

    Add support card component

---
+++ src/components/SupportCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SupportCard() {
+  return (
+    <div>
+      <h3>Support</h3>
+      <p>Mentor meetings, well-being support, MedOpp advising, and CASE support.</p>
+    </div>
+  );
+}

9a6df62c85436cb5e8c519cd730e6b502621601b
drjstancer <drj.stancer@gmail.com>
2026-06-19 13:32:36 -0500

    Add intervention card component

---
+++ src/components/InterventionCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function InterventionCard() {
+  return (
+    <div>
+      <h3>Intervention</h3>
+      <p>Alerts, follow-ups, open concerns, and resolution tracking.</p>
+    </div>
+  );
+}

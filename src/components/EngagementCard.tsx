9d7f74cc40021eba51d5674f2f2c68912e3285ca
drjstancer <drj.stancer@gmail.com>
2026-06-19 13:29:10 -0500

    Add engagement card component

---
+++ src/components/EngagementCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function EngagementCard() {
+  return (
+    <div>
+      <h3>Engagement</h3>
+      <p>Attendance, seminars, workshops, retreats, and cohort meetings.</p>
+    </div>
+  );
+}

38babb6ae7f95f23e7e1fd02f0d89cff1cffff43
drjstancer <drj.stancer@gmail.com>
2026-06-19 13:31:59 -0500

    Add development card component

---
+++ src/components/DevelopmentCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function DevelopmentCard() {
+  return (
+    <div>
+      <h3>Development</h3>
+      <p>Shadowing, clinical, research, service, leadership, and professional development.</p>
+    </div>
+  );
+}
